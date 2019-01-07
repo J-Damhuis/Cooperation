0a1d19b11455901c9e4d2de88df08ef5996e0a5a
J-Damhuis <j.damhuis.1@student.rug.nl>
2019-01-07 16:47:35 +0100

    EOD

--- R/R-plots.R
+++ R/R-plots.R
@@ -1,24 +1,11 @@
 library(ggplot2)
 
-d <- read.csv(file = "R/Model 1.1.csv", header = FALSE, sep = "\t")
+d <- read.csv(file = "Model 0.1.csv", header = FALSE, sep = "\t")
+d <- `colnames<-`(d, c("Generation", "P01", "Stdev1", "P02", "Stdev2", "P03", "Stdev3"))
 
-ggplot(data = d, aes(x = d[1])) + geom_point(aes(y = d[2], colour = "P0 = 0.95"), size = 0.5) + 
-  geom_point(aes(y = d[6], colour = "P0 = 0.05"), size = 0.5) + 
-  geom_point(aes(y = d[10], colour = "P0 = 0.67"), size = 0.5) +
-  labs(x = "Generation", y = "Fraction of interactions where focal individual cooperates") +
-  scale_colour_manual(values = c("orange", "grey", "blue")) +
-  xlim(0, 1000000) + ylim(0.0, 1.0) + theme(legend.title=element_blank())
-
-ggplot(data = d, aes(x = d[1])) + geom_point(aes(y = d[3], colour = "P0 = 0.95"), size = 0.5) + 
-  geom_point(aes(y = d[7], colour = "P0 = 0.05"), size = 0.5) + 
-  geom_point(aes(y = d[11], colour = "P0 = 0.67"), size = 0.5) +
-  labs(x = "Generation", y = "Mean P0 value") +
-  scale_colour_manual(values = c("orange", "grey", "blue")) +
-  xlim(0, 1000000) + ylim(0.0, 1.0) + theme(legend.title=element_blank())
-
-ggplot(data = d, aes(x = d[1])) + geom_point(aes(y = d[5], colour = "P0 = 0.95"), size = 0.5) + 
-  geom_point(aes(y = d[9], colour = "P0 = 0.05"), size = 0.5) + 
-  geom_point(aes(y = d[13], colour = "P0 = 0.67"), size = 0.5) +
-  labs(x = "Generation", y = "Fraction of population which obtains information") +
-  scale_colour_manual(values = c("orange", "grey", "blue")) +
-  xlim(0, 1000000) + ylim(0.0, 1.0) + theme(legend.title=element_blank())
+ggplot(data = d, aes(x = Generation)) + 
+  geom_point(aes(y = P01, colour = "High"), size = 2) + 
+  geom_point(aes(y = P02, colour = "Low"), size = 2) + 
+  geom_point(aes(y = P03, colour = "Medium"), size = 2) +
+  labs(x = "Generation", y = "Mean P0", colour = "Cooperativeness") + 
+  xlim(0, 10000) + ylim(0.0, 1.0)
