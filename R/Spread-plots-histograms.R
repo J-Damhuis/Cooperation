fcf90ba36b5788b988e1c545728f2d37decf92a1
J-Damhuis <j.damhuis.1@student.rug.nl>
2019-01-08 13:50:25 +0100

    Model 1.1 summary

--- R/Spread-plots-histograms.R
+++ R/Spread-plots-histograms.R
@@ -1,11 +1,10 @@
-setwd("C:/Users/annad/Cooperation/Data/Spread_csv_files")
+setwd("C:/Users/FSE-Education/Cooperation/Data/Model 1.1")
 
 library(ggplot2)
 
-filename <- "spread_model1.2_0.67.csv"
+filename <- "spread_model1.1_0.05.csv"
 
 d <- read.csv(file = filename, header = FALSE, sep = "\t")
-d <- d[1:101,]
 
 p <- ggplot(data = d, aes(x = d$V1))
 
@@ -17,7 +16,8 @@ for (i in 2:length(d[,1])) {
 p + ylim(0.0, 1.0) + ggtitle(filename)
 
 d2 <- d[length(d[,1]),]
-d2 <- as.data.frame(t(d2)[-1,])
+d2 <- as.data.frame(t(d2))
+d2 <- as.data.frame(d2[-1,])
 colnames(d2) <- "value"
 
 ggplot(d2, aes(x = value)) + geom_histogram(bins = 100) + ggtitle(filename)
